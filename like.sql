6f4e022a0d2c616bd94230598c3e7c03f6188ad1
loipham123 <phuocloi100204@gmail.com>
2025-10-31 23:00:18 +0700

    fix comment

--- like.sql
+++ like.sql
@@ -16,5 +16,6 @@ select * from hoc_sinh where char_length(ho_ten_hs) <= 30 and ho_ten_hs like 'N%
 -- h. Những học sinh có họ tên bắt đầu bằng các ký tự: N, T, V.
 select * from hoc_sinh where ho_ten_hs like 'N%' or ho_ten_hs like 'T%' or ho_ten_hs like 'V%';
 -- i. Những học sinh có họ tên không được bắt đầu bằng các ký tự: N, T, V.
-
+select * from hoc_sinh where ho_ten_hs not like 'N%' and ho_ten_hs not like 'T%' and ho_ten_hs not like 'V%';
 -- j. Những học sinh có họ tên với phần họ có đúng 4 ký tự.
+select * from hoc_sinh where substring_index(ho_ten_hs,' ',1) like '____';
